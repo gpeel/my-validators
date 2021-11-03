6062007f6e2ae373b3e22a88155380a023aa4ca6
Gauthier PEEL <gauthier.peel@gmail.com>
2021-11-03 17:03:18 +0100

    again

--- projects/reactive1-flexible-ui/src/app/reactive-flexible-ui/reactive-ui.component.ts
+++ projects/reactive1-flexible-ui/src/app/reactive-flexible-ui/reactive-ui.component.ts
@@ -30,18 +30,16 @@ export class ReactiveUiComponent implements OnInit {
 
   test4 = `
     extraMessagesOverride: ErrorMsgMap = {
-    required: 'Override You must choose something !',
-    minlength: ({
-                  actualLength,
-                  requiredLength
-                }) => \`Override : Actual length is \${actualLength} and we need \${requiredLength}!\`
-  };
+      required: 'Override You must choose something !',
+      minlength: ({
+                    actualLength,
+                    requiredLength
+                  }) => \`Override : Actual length is \${actualLength} and we need \${requiredLength}!\`
+    };
   `;
 
   test5 = ` 
-  You can add the parameter myErrorClass in the <input> as 
   <input  myErrorClass="my-error-CUSTOM" myErrorMsg>
-  And define a global style in style.(s)css:
   
   .my-error-CUSTOM {
     font-style: italic;
@@ -50,10 +48,10 @@ export class ReactiveUiComponent implements OnInit {
   `;
 
   test6 = `
-                <my-error-msg #name6Errors="myErrorMsg" 
-                            myErrorClass="my-error-CUSTOM"
-                            [myErrorExtraMsg]="extraMessagesOverride"
-              ></my-error-msg>
+        <my-error-msg #name6Errors="myErrorMsg" 
+                    myErrorClass="my-error-CUSTOM"
+                    [myErrorExtraMsg]="extraMessagesOverride"
+      ></my-error-msg>
   `;
 
   constructor(private fb: FormBuilder) {
